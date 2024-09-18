346cf699ff9ae46512d4f003a1bbb17fd8056aff
yahyashanaah <yahya.shanaah@gmail.com>
2024-09-18 23:23:21 +0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+})
+export class AppComponent {
+
+}
