251f2cddec5936a8dd31099f45adb71ba70c1601
yahyashanaah <yahya.shanaah@gmail.com>
2024-09-29 00:15:51 +0400

    button 2024929

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
