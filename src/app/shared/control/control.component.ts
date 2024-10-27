7f88edbc7cedf123300da62774a853fe3460e712
yahyashanaah <yahya.shanaah@gmail.com>
2024-10-27 21:27:10 +0400

    host

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component,ViewEncapsulation ,input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
